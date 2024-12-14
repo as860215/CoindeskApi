0f1bfbb7d3c17916fc9696105ab2561c3b38cbaf
as860215 <shana860215@gmail.com>
2024-12-14 15:28:42 +0800

    新增JWT

---
+++ Utility.Security.Test/JwtAuthTest.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Utility.Security.Test;
+public class JwtAuthTest
+{
+    private const string key = "capoo";
+
+    [Fact]
+    public void GenerateToken()
+    {
+        var result = new JwtAuth().GenerateToken(key, "啊啊啊啊啊啊啊啊");
+        result.Should().Be("eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJJZGVudGlmaWNhdGlvbiI6IuWViuWViuWViuWViuWViuWViuWViuWViiJ9.Fq55p1aTuNsw5SnGaa6uY2MAFK8xMigWREWbS7fTW8Y6T7LEG997c2fiQTKbILj2QTsc9l_P3ttXLuHpYC0NeA");
+    }
+
+    [Fact]
+    public void AuthorizationToken()
+    {
+        var token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJJZGVudGlmaWNhdGlvbiI6Iueyiee0heefruWGrOeTnCJ9.jR-qmPS9lJ_QgkcCtFZHyX8HvNiVscXLhojqKCRAKjTBr5Gq3N4-fmYm_IugYdC_zeKE1TCzu0VQIOnznW1nGQ";
+        var result = new JwtAuth().AuthorizationToken(key, token);
+
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void TokenHasTimeout_Alive()
+    {
+
+        var token = new JwtAuth().GenerateToken(key, "我還沒過期", 100);
+        var result = new JwtAuth().AuthorizationToken(key, token);
+
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void TokenHasTimeout_NotAlive()
+    {
+
+        var token = new JwtAuth().GenerateToken(key, "我過期了", -100);
+        var result = new JwtAuth().AuthorizationToken(key, token);
+
+        result.Should().BeFalse();
+    }
+}
---
+++ Utility.Security/JwtAuth.cs
@@ -0,0 +1,57 @@
+﻿using Jose;
+using System.Text;
+
+namespace Utility.Security
+{
+    /// <summary>JWT授權機制</summary>
+    public class JwtAuth
+    {
+        /// <summary>到期時間</summary>
+        private const string expirationTimeTag = "Exp";
+        /// <summary>識別項</summary>
+        private const string identificationTag = "Identification";
+
+        /// <summary>產生Token</summary>
+        /// <param name="key">鍵值</param>
+        /// <param name="identification">識別項</param>
+        /// <param name="timeout">到期時間（NULL代表無限期）</param>
+        public string GenerateToken(string key, string identification, int? timeout = null)
+        {
+            ArgumentNullException.ThrowIfNull(key);
+            ArgumentNullException.ThrowIfNull(identification);
+
+            var payload = new Dictionary<string, string>
+            {
+                { identificationTag, identification }
+            };
+            if(timeout != null)
+                payload.Add(expirationTimeTag, DateTimeOffset.Now.AddSeconds(timeout.Value).ToString());
+
+            var token = JWT.Encode(payload, Encoding.UTF8.GetBytes(key), JwsAlgorithm.HS512);
+            return token;
+        }
+
+        /// <summary>驗證Token</summary>
+        /// <param name="key"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public bool AuthorizationToken(string key, string token)
+        {
+            if (key == null || token == null) return false;
+
+            var jwtObject = JWT.Decode<Dictionary<string, string>>(token, Encoding.UTF8.GetBytes(key), JwsAlgorithm.HS512);
+            if (!jwtObject.ContainsKey(identificationTag))
+                return false;
+
+            if (jwtObject.ContainsKey(expirationTimeTag))
+            {
+                if (DateTimeOffset.TryParse(jwtObject[expirationTimeTag], out var time))
+                    return time > DateTimeOffset.Now;
+                else
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
